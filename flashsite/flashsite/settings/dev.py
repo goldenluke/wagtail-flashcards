a2955af73ca92cbc4302749f4d3ec35447681597
Lucas Dourado <lucasdourado.d@gmail.com>
2024-11-20 14:14:00 -0300

    Install Wagtail

---
+++ flashsite/flashsite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-7hy=hllc5=cwy5e=4u9e08r9(@4*nfm$b!zyfrov1b-1gr%$ph"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
